b0e877caacedf6eeeaf6734356f0541b0e2d61aa
Roman Sklenář <mail@romansklenar.cz>
2015-04-15 16:04:04 +0200

    Use decorated resources in Admin

--- app/admin/position.rb
+++ app/admin/position.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Position do
+  decorate_with PositionDecorator
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
