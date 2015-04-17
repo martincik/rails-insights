611a13316d79ee0dc9aa4c8dd72e6e1d98d90b65
Roman Sklenář <mail@romansklenar.cz>
2015-04-17 14:07:19 +0200

    Improved Admin::Positions#index view

--- app/admin/position.rb
+++ app/admin/position.rb
@@ -5,4 +5,23 @@ ActiveAdmin.register Position do
   actions :all, except: [:new]
 
   permit_params :identifier, :company_id, :portal_id, :title, :description, :how_to_apply, :url, :location, :salary, :kind, :type, :posted_at, :synchronized_at
+
+  index do
+    selectable_column
+    column :id do |position| auto_link position, position.id end
+    column :identifier
+    column :title
+    column :description, sortable: :description_text
+    column :how_to_apply
+    column :url
+    column :location
+    column :salary
+    column :kind
+    column :type
+    column :posted_at
+    column :synchronized_at
+    column :created_at
+    column :updated_at
+    actions
+  end
 end
--- app/decorators/position_decorator.rb
+++ app/decorators/position_decorator.rb
@@ -1,3 +1,9 @@
 class PositionDecorator < ApplicationDecorator
   delegate_all
+
+  def description_text(length: 150)
+    truncate object.description_text, length: length, separator: ' ', escape: false, omission: '... (continued)' if object.description_text.present?
+  end
+
+  alias_method :description, :description_text
 end
