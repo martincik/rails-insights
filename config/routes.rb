6dd66e0091a29429b73eb8a13a189315d3f41073
Roman Sklenář <mail@romansklenar.cz>
2015-04-20 13:46:50 +0200

    Specified root of application to admin dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
+
+  root 'admin/dashboard#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
