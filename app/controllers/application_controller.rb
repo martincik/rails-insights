9655e3ced0812cb1df1588f00685f5bc75a11ac9
Roman Sklenář <mail@romansklenar.cz>
2015-04-20 13:01:38 +0200

    Ensure domain on production and staging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :restrict_access, if: -> { %w[production staging].include?(Rails.env) && Rails.application.secrets.http_basic_auth.present? }
+  before_action :ensure_domain,   if: -> { %w[production staging].include?(Rails.env) && ENV['FORWARD_DOMAIN'].present? }
+  before_action :restrict_access, if: -> { %w[production staging].include?(Rails.env) && ENV['RESTRICT_ACCESS'].present? }
 
   protected
+
+  def ensure_domain
+    domain = Rails.application.secrets.domain_name
+    if domain && !domain.include?('heroku') && request.env['HTTP_HOST'].include?('heroku')
+      redirect_to host: domain, status: 301
+    end
+  end
 
   def restrict_access
     authenticate_or_request_with_http_basic do |username, password|
