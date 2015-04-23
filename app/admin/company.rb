757613d420190fae98cc318d90866c5636f1f94a
Roman Sklenář <mail@romansklenar.cz>
2015-04-23 16:05:42 +0200

    Modified Admin::Companies#index using decorator

--- app/admin/company.rb
+++ app/admin/company.rb
@@ -5,4 +5,15 @@ ActiveAdmin.register Company do
   actions :all, except: [:new]
 
   permit_params :name, :homepage_domain, :homepage_url, :logo_url
+
+  index do
+    selectable_column
+    column :id do |company| auto_link company, company.id end
+    column :logo_image, sortable: :logo_url
+    column :name
+    column :homepage_url
+    column :created_at
+    column :updated_at
+    actions
+  end
 end
--- app/decorators/company_decorator.rb
+++ app/decorators/company_decorator.rb
@@ -1,3 +1,11 @@
 class CompanyDecorator < ApplicationDecorator
   delegate_all
+
+  def homepage_url
+    link_to_unless object.homepage_url.blank?, object.homepage_domain, object.homepage_url, target: '_blank' if object.homepage_url.present?
+  end
+
+  def logo_image
+    image_tag object.logo_url, width: 64 if object.logo_url.present?
+  end
 end
