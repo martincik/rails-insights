5e66a747ad84ceffe8d773e5613210eec31f0ddd
Roman Sklenář <mail@romansklenar.cz>
2015-04-27 09:04:13 +0200

    Refactored crawler factory class

--- app/services/crawler/position/factory.rb
+++ app/services/crawler/position/factory.rb
@@ -2,16 +2,22 @@ module Crawler
   module Position
 
     class Factory
-      attr_reader :portal
+      attr_reader :position
 
-      def initialize(domain)
-        @portal = Portal.where("domain ILIKE ?", "%#{domain}%").first
-        raise Crawler::UnknownPortalError, "Unable to find portal for domain #{domain}" if @portal.nil?
+      def initialize(position)
+        @position = position
       end
 
-      def instance(position)
-        raise Crawler::UnknownCrawlerError, "Unknown crawler for portal #{@portal.name}" if @portal.crawler_class.nil?
-        @portal.crawler_class.camelize.constantize.new(position, portal: @portal)
+      def instance
+        raise Crawler::UnknownPortalError, "Unable to find portal for domain #{position.domain}" if portal.nil?
+        raise Crawler::UnknownCrawlerError, "Unknown crawler for portal #{portal.name}" if portal.crawler_class.nil?
+        @portal.crawler_class.camelize.constantize.new(position, portal: portal)
+      end
+
+      protected
+
+      def portal
+        @portal ||= (position.portal || Portal.where("domain ILIKE ?", "%#{position.domain}%").first)
       end
     end
 
