a161631f925e38ea95a7587cfbff6ad8a9c0f884
Roman Sklenář <mail@romansklenar.cz>
2015-04-29 10:31:39 +0200

    Implemented crawler of jobsremotely.com portal

--- app/services/crawler/position/jobs_remotely.rb
+++ app/services/crawler/position/jobs_remotely.rb
@@ -2,6 +2,23 @@ module Crawler
   module Position
 
     class JobsRemotely < Base
+      def crawle!
+        page = html.css('body')
+        name = page.css('.well.col-sm-4 .col-sm-12 h3 a').xpath('text()').text.squish.strip.presence rescue nil
+
+        @position.transaction do
+          @position.portal  ||= @portal
+          @position.company ||= Company.where(name: name).first_or_create!
+          @position.company.homepage_url = page.css('.well.col-sm-4 .col-sm-12:nth-child(2) a').attr('href').value.strip.presence rescue nil
+          @position.company.logo_url     = page.css('.well.col-sm-4 .col-sm-12:nth-child(1) img').attr('src').value.strip.presence rescue nil
+          @position.description_text     = page.css('.well.col-sm-8 .col-sm-12').first.inner_text.strip
+          @position.description_html     = page.css('.well.col-sm-8 .col-sm-12').first.inner_html.strip
+          @position.how_to_apply         = page.css('.well.col-sm-8 .col-sm-12').last.inner_html.strip
+          @position.title                = page.css('.jumbotron .text-center h1').text.strip.presence
+          @position.location             = page.css('.jumbotron .text-center small').text.split('/').last.gsub('location', '').strip.presence rescue nil
+          @position.save(validate: false)
+        end
+      end
     end
 
   end
