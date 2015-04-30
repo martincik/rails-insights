ecec298d216079fe443e8c9294167d276885df92
Roman Sklenář <mail@romansklenar.cz>
2015-04-30 11:34:25 +0200

    Modified User resource detail

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -17,6 +17,32 @@ ActiveAdmin.register User do
     actions
   end
 
+  show do
+    attributes_table do
+      row :id
+      row :name
+      row :email
+
+      # Timestampable
+      row :created_at
+      row :updated_at
+    end
+
+    panel I18n.t("active_admin.panels.login_details"), collapsed: true do
+      attributes_table_for user.decorate do
+        # trackable
+        row :current_sign_in_at
+        row :current_sign_in_ip
+        row :last_sign_in_at
+        row :last_sign_in_ip
+        row :sign_in_count
+
+        # rememberable
+        row :remember_created_at
+      end
+    end
+  end
+
   filter :name
   filter :email
   filter :current_sign_in_at
