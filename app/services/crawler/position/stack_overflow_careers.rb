dce5ac255b345718583c4423e6a6b93b3ba14ad2
Roman Sklenář <mail@romansklenar.cz>
2015-05-28 12:31:14 +0200

    Implemented ability to detect archived jobs for Stackoverflow portal

--- app/services/crawler/position/stack_overflow_careers.rb
+++ app/services/crawler/position/stack_overflow_careers.rb
@@ -6,18 +6,23 @@ module Crawler
         page = html.css('#jobdetailpage')
         name = page.css('a.employer').xpath('text()').text.squish.strip.presence rescue nil
 
-        @position.transaction do
-          @position.portal  ||= @portal
-          @position.company ||= Company.where(name: name).first_or_create!
-          @position.company.homepage_url = page.css('a.employer').attr('href').value.strip.presence rescue nil
-          @position.company.logo_url     = page.css('.company .-logo img').attr('src').value.strip.presence rescue nil
-          @position.description_text     = page.css('.h4, .description').inner_text.strip
-          @position.description_html     = page.css('.h4, .description').inner_html.strip
-          @position.how_to_apply         = 'Apply using StackOverflow'
-          @position.title                = page.css('.jobdetail h1').text.strip.presence
-          @position.location             = page.css('.jobdetail .location').text.gsub('(allows remote)', '').strip.presence
-          @position.kind                 = 'remote' if page.css('.jobdetail .location').text.downcase.include?('remote').presence
+        if page.none?
+          @position.archived = true unless @position.archived?
           @position.save(validate: false)
+        else
+          @position.transaction do
+            @position.portal  ||= @portal
+            @position.company ||= Company.where(name: name).first_or_create!
+            @position.company.homepage_url = page.css('a.employer').attr('href').value.strip.presence rescue nil
+            @position.company.logo_url     = page.css('.company .-logo img').attr('src').value.strip.presence rescue nil
+            @position.description_text     = page.css('.h4, .description').inner_text.strip
+            @position.description_html     = page.css('.h4, .description').inner_html.strip
+            @position.how_to_apply         = 'Apply using StackOverflow'
+            @position.title                = page.css('.jobdetail h1').text.strip.presence
+            @position.location             = page.css('.jobdetail .location').text.gsub('(allows remote)', '').strip.presence
+            @position.kind                 = 'remote' if page.css('.jobdetail .location').text.downcase.include?('remote').presence
+            @position.save(validate: false)
+          end
         end
       end
     end
