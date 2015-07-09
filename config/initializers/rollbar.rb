0a73c558e4eb52b21e7c4be1954bcb9189babf4e
Roman Sklenář <mail@romansklenar.cz>
2015-07-09 14:49:50 +0200

    Configured rollbar

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -1,23 +1,28 @@
 require 'rollbar/rails'
+
 Rollbar.configure do |config|
   # Without configuration, Rollbar is enabled in all environments.
   # To disable in specific environments, set config.enabled=false.
-  
-  config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
 
-  # Here we'll disable in 'test':
-  if Rails.env.test?
+  secrets = Rails.application.secrets.rollbar.symbolize_keys
+  config.access_token = secrets[:access_token]
+
+  # Here we'll disable in 'test' and 'development'
+  if Rails.env.test? || Rails.env.development?
     config.enabled = false
   end
-  
+
 
   # By default, Rollbar will try to call the `current_user` controller method
   # to fetch the logged-in user object, and then call that object's `id`,
   # `username`, and `email` methods to fetch those properties. To customize:
-  # config.person_method = "my_current_user"
-  # config.person_id_method = "my_id"
-  # config.person_username_method = "my_username"
-  # config.person_email_method = "my_email"
+  config.person_method = "current_user"
+  config.person_id_method = "id"
+  config.person_username_method = "name"
+  config.person_email_method = "email"
+
+  # scrub out the "X-Access-Token" http header
+  config.scrub_headers |= ["X-Access-Token", "X-Storageapi-Token"]
 
   # If you want to attach custom data to all exception and message reports,
   # provide a lambda like the following. It should return a hash.
